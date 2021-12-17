77e4e84c4a52e971ad2f488e9dd677b57d5af18d
LucasToisso <70295622+LucasToisso@users.noreply.github.com>
2021-12-17 00:51:25 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,17 @@
 
+function verificaSoma(){
+    //calculo.innerHTML = +document.getElementById('primeiro').value + +document.getElementById('segundo').value;
+    calculo.innerHTML = Number(document.getElementById('primeiro').value) + Number(document.getElementById('segundo').value);
+}
+
+function verificaSubt(){
+    calculo.innerHTML = document.getElementById('primeiro').value - document.getElementById('segundo').value;
+}
+
+function verificaDivi(){
+    calculo.innerHTML = document.getElementById('primeiro').value / document.getElementById('segundo').value;
+}
+
+function verificaMult(){
+    calculo.innerHTML = document.getElementById('primeiro').value * document.getElementById('segundo').value;
+}
